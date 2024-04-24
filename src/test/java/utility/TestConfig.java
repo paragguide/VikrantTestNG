c5c4fea629549977d4e74f55cba03f5631800521
paragguide <paragguide@yahoo.co.in>
2024-04-24 15:51:00 +0530

    hello

---
+++ src/test/java/utility/TestConfig.java
@@ -0,0 +1,22 @@
+
+package utility;
+
+public class TestConfig{
+
+
+	
+	public static String server="smtp.gmail.com";
+	public static String from = "roliguide@gmail.com";
+	public static String password = "omhwiqsuswbbseah";
+	public static String[] to ={"paragguide@yahoo.co.in","roliguide@gmail.com","vikrantagarwal882@gmail.com"};
+	public static String subject = "Report ";
+	
+	public static String messageBody ="report of project 10-4-24 TestMessage";
+	//public static String attachmentPath= System.getProperty("user.dir")+"\\src\\test\\java\\reports\\amazon.html";
+	
+	//public static String attachmentName="amazon.html";
+	
+	
+	
+		
+}
